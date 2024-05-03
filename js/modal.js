1d33d55bff3da9019571ba766ebc9e30cdc6de35
Abdulwarith <adebayoayomide646@gmail.com>
2024-05-03 20:45:34 +0100

    updated chatbot

--- js/modal.js
+++ js/modal.js
@@ -8,11 +8,19 @@ const baseUrl = "https://chatterbot-ordering-fastapi.onrender.com"
 
 // Function to add a message to the chat container
 function addMessage(sender, message, isUser) {
+  const menu = message.menu && message.menu.map((item) => {
+    return `<li><strong>${item.name}: </strong> ${item.price}</li>`
+  })
   const messageDiv = document.createElement("div");
   const messageClass = isUser ? "user-message" : "bot-message";
   messageDiv.classList.add("chat-message");
   messageDiv.classList.add(messageClass);
-  messageDiv.innerHTML = "<strong>" + sender + ": </strong>" + message;
+  messageDiv.innerHTML =
+    message.response && !message.order?.order_id
+      ? `<strong> ${sender} : </strong> ${message.response}`
+      : message.order?.order_id
+      ? `<strong> ${sender} : </strong>Your order has been placed successfully and your Order Id is <strong>${message.order.order_id}</strong>`
+      : message.menu && `The available menu are<ol>${menu.join(' ')}</ol>`;
   chatContainer.appendChild(messageDiv);
   // Scroll to the bottom of the chat container
   chatContainer.scrollTop = chatContainer.scrollHeight;
@@ -22,17 +30,25 @@ function addMessage(sender, message, isUser) {
 async function handleUserInput() {
   const userMessage = userInput.value.trim();
   if (userMessage !== "") {
-    addMessage("You", userMessage, true);
+    const msg = {
+      response: userMessage
+    }
+    addMessage("You", msg, true);
     const payload = {
       user_input: userMessage
     }
-     const res = await axios.post(`${baseUrl}/chat`, payload);
-    // Here you could send the user's message to a server for processing by a bot,
-    // and then receive and display the bot's response.
-    // For this example, let's simulate a bot response after a short delay
-      addMessage("Bot", res.data.response, false);
 
-    userInput.value = ""; // Clear the input field after sending the message
+    try {
+      const res = await axios.post(`${baseUrl}/chat`, payload);
+      // Here you could send the user's message to a server for processing by a bot,
+      // and then receive and display the bot's response.
+      // For this example, let's simulate a bot response after a short delay
+      addMessage("Bot", res.data, false);
+
+      userInput.value = ""; // Clear the input field after sending the message
+    } catch (error) {
+      alert(error.response.data.detail.response)
+    }
   }
 }
 
